8f9264c1ddab984257f7ca3726757ed4ca90daaf
Alvaro Mongil <alvaro.mongil@gmail.com>
2026-08-07 14:40:36 +0200

    feat(routes): primera ruta establecida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './views/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+];
