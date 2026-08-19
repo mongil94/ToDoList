e8af5ccbf74e4ef780df116618b7008c0f10ace3
Alvaro Mongil <alvaro.mongil@gmail.com>
2026-08-19 16:10:30 +0200

    feat(login): añadida funcionalidad de mostrar contraseña

--- src/app/views/login.ts
+++ src/app/views/login.ts
@@ -1,19 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { TranslatePipe } from '@ngx-translate/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { Subject, takeUntil } from 'rxjs';
+import { TypeInputPass } from '../enums/type-input-pass.enum';
 
 @Component({
   selector: 'login',
   templateUrl: './login.html',
   styleUrls: ['./login.scss'],
-  imports: [InputTextModule, PanelModule, CheckboxModule, FloatLabelModule, TranslatePipe],
+  imports: [
+    InputTextModule,
+    PanelModule,
+    CheckboxModule,
+    FloatLabelModule,
+    TranslatePipe,
+    ReactiveFormsModule,
+  ],
   standalone: true,
 })
-export class LoginComponent {
-  public changeVisibilityPass(){
-    
+export class LoginComponent implements OnInit, OnDestroy {
+  public typeInput = TypeInputPass.PASSWORD;
+  public loginForm = new FormGroup({
+    pass: new FormControl<string>(''),
+    checkPass: new FormControl<boolean>(false),
+  });
+  private _destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.loginForm
+      .get('checkPass')
+      ?.valueChanges.pipe(takeUntil(this._destroy$))
+      .subscribe((newValue) => {
+        console.log(newValue);
+        
+         this.typeInput =newValue ? TypeInputPass.TEXT : TypeInputPass.PASSWORD;
+      });
+  }
+  ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 }
